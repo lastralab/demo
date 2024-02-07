4addb75fdf6a01996e669026ce44dc39ab333c1c
lastralab <tania@lastralab.com>
2024-02-07 15:03:12 -0500

    [TAN-0007] Created theme placeholder and minimum customizations

---
+++ app/code/Tan/InitTheme/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: tan
+ * Project: Demo
+ * Date: 02/06/2024
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Tan_InitTheme',
+    __DIR__);
---
+++ app/design/frontend/Tan/theme/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: tan
+ * Project: Demo
+ * Date: 02/01/2024
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Tan/theme', __DIR__);
